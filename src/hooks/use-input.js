aad79cbfb45b7eb32907fbe24111556d08d39188
Sammydepoj <samueladepoju86@yahoo.com>
2023-02-05 11:35:44 +0100

    replace use reducer with usestate in the custom input hook

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,25 +1,55 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === "RESET") {
+    return { isTouched: false, value: "" };
+  }
+  return initialInputState;
+};
+
 const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+
+  //   const [enteredValue, setEnteredValue] = useState("");
+  //   const [isTouched, setIsTouched] = useState(false);
+  //   const valueIsValid = validateValue(enteredValue);
+  const valueIsValid = validateValue(inputState.value);
+  //   const hasError = !valueIsValid && isTouched;
+  const hasError = !valueIsValid && inputState.isTouched;
 
   const valueChangeHandler = (event) => {
-    setEnteredValue(event.target.value);
+    dispatch({ type: "INPUT", value: event.target.value });
+    // setEnteredValue(event.target.value);
   };
 
   const inputBlurHandler = (event) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
+    // setIsTouched(true);
   };
 
   const reset = () => {
-    setEnteredValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
+    // setEnteredValue("");
+    // setIsTouched(false);
   };
 
   return {
-    value: enteredValue,
+    // value: enteredValue,
+    value: inputState.value,
     hasError,
     isValid: valueIsValid,
     valueChangeHandler,
